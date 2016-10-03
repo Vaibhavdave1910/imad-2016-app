941ce0a571347976ac58434c42a55a87064678ef
Vaibhavdave1910 <Vaibhav.dave1910@gmail.com>
2016-10-03 08:51:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/maxresdefault.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'maxresdefault.jpg'));
+app.get('/Pokemon-PNG-HD.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'Pokemon-PNG-HD.png'));
 });
 
 
