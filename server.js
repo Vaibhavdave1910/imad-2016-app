9827cff992149d108fc82f793b7e6d65fd341d46
Vaibhavdave1910 <Vaibhav.dave1910@gmail.com>
2016-10-05 20:14:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,58 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-article-one:{ title:'article one|vaibhav dave',
-    heading:'article one',
-    content:`<p>this is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web site</p>
-            
-            <p>this is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web site</p>
-            
-            <p>this is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web site</p>`
-},
-article-two:{title:'article two|vaibhav dave',
-    heading:'article two',
-    content:`<p>this is my second web site</p>`
-    
-},
-article-three:{title:'article two|vaibhav dave',
-    heading:'article two',
-    content:`<p>this is my second web site</p>`
-    },
-};
-    
-    
-function createTemplate(data){
-    var title=data.title;
-    var heading=data.heading;
-    var content=data.content;
-var htmlTemplate =
-`  <html>
-    <head>
-        <title>
-            ${title}
-        </title>
-         <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href="/">home</a>
-        </div>
-        <hr/>
-        
-        <div>
-            <h3> ${heading}<h3>
-        </div>
-        <div>
-           ${content}
-        </div>
-        </div>
-    </body>
-</html>`;
-return htmlTemplate;
 
-}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -78,9 +27,6 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/eena', function(req,res) {
-    res.send("joy");
-});
 app.get('/Pokemon-PNG-HD.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'Pokemon-PNG-HD.png'));
 });
