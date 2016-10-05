1d4084192bd136bd95c9d87443b0a15fe482373f
Vaibhavdave1910 <Vaibhav.dave1910@gmail.com>
2016-10-05 20:52:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,18 +15,44 @@ var articleOne={
             <p>this is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web sitethis is my first web site</p>`
 };
 
+function createTemlate(data){
+ var title=data.title;
+ var heading=data.heading;
+ var content=data.content;
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">home</a>
+        </div>
+        <hr/>
+        
+        <div>
+            <h3>${heading}</h3>
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>`;
+return htmlTemplate;
 
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/article-one',function(req,res){
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/ui/style.css', function (req, res) {
