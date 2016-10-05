61bfb7dd156d15577ba6e9117a2533df54465219
Vaibhavdave1910 <Vaibhav.dave1910@gmail.com>
2016-10-05 13:05:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
+app.get('/article-one', function (req,res) {
+    res.send('ai cha gavat') ;
+});
+
+app.get('/article-two',function(req,res){
+    res.send('ai cha gavat');
+});
+
+app.get('/article-three',function(req,res){
     res.send('ai cha gavat');
 });
 
