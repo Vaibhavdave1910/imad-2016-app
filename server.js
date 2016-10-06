21ae37ef0ab0f38dd7186dc5f3b7bc45898db52f
Vaibhavdave1910 <Vaibhav.dave1910@gmail.com>
2016-10-06 17:32:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ return htmlTemplate;
 }
 
 app.get('/:articleName',function(req,res){
-    var articalName = req.params.articalName;
+    var articalName = req.paramas.articalName;
     res.send(createTemplate(articles[articalName]));
 });
 
