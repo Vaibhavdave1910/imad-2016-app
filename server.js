9a22464d0f4c02f83ea0bead95de168a6883058f
Vaibhavdave1910 <Vaibhav.dave1910@gmail.com>
2016-10-06 13:33:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ function createTemplate(data){
 var htmlTemplate=`<html>
     <head>
         <title>
-        ${title}
+                 ${title}
         </title>
          <link href="/ui/style.css" rel="stylesheet" />
     </head>
