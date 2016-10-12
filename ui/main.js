cf757306a29dddbb0d10319c4653189247dedc75
Vaibhavdave1910 <Vaibhav.dave1910@gmail.com>
2016-10-12 19:48:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,13 @@ var element= document.getElementById('main text');
 element.innerHTML="new value";
   
 var img=document.getElementById('img');
+var marginLeft=0;
+function moveRight () {
+  marginLeft = marginLeft+10;    
+  img.style.marginLeft= marginLeft + 'px';
+    
+}
 img.onclick=function (){
+    var interval=setInterval(moveRight,100);
   img.style.marginLeft='100px';  
 };
