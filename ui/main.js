9de22139cc37e3eddbeb1051e8eae24cd6d73c51
Vaibhavdave1910 <Vaibhav.dave1910@gmail.com>
2016-10-12 20:18:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,6 @@ console.log('Loaded!');
 console.log('asdf');
 var element = document.getElementById('main text');
 element.innerHTML=('hi all');
+
+var img = document.getElementById('img');
+img.style.marginLeft='100px';
